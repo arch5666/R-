7ace9b1f118772c6ad96c0d264be15651c14df10
arch5666 <halla910372@gmail.com>
2019-12-04 15:16:58 +0900

    Update 6.R

--- 6.R
+++ 6.R
@@ -79,6 +79,128 @@ sum(is.na(x))
 
 
 
+# 결측치 제외한 새로운 데이터셋 생성
+head(x)
+x[ !complete.cases(x), ]  # NA가 포함된 행출력
+y <- x[ complete.cases(x), ]
+head(y)
+
+
+# 이상치 (outlier)
+# 1. 논리적으로 성립되지 않는 값 
+# 2. 상식적으로 용인되지 않는값
+# boxplot.stats 이걸 쓰든가 summary를 써야됨
+
+# 특이값 , 이상치 (outlier)
+st <- data.frame(state.x77)
+summary(st$Income)
+boxplot(st$Income)
+boxplot.stats(st$Income)$out
+
+
+
+# 특이값 처리 %in% 이건 포함되어있다면! 
+# 데이터 정제!
+out.val <- boxplot.stats(st$Income)$out
+st$Income[st$Income %in% out.val] <- NA
+head(st)
+newdata <- st[ complete.cases(st), ]
+head(newdata)
+
+boxplot(newdata$Income)
+
+#
+#
+# 데이터 가공
+#
+# 데이터 정렬
+# Vector 정렬
+# 
+# sort 는 간단하게 순서 벡터할때
+# order는 명확하게 순서 정할때
+# split는 내부적으로 같은거끼리 분류할때
+
+v1 <- c(1,7,6,8,4,2,3)
+order(v1)
+v1 <- sort(v1)
+v1
+v2 <- sort(v1, decreasing = T)
+v2
+
+# Matrix/ Data frame 정렬
+
+head(iris)
+order(iris$Sepal.Length)
+iris[order(iris$Sepal.Length),] # Ascending
+iris[order(iris$Sepal.Length,decreasing = T), ]
+iris.new <- iris[order(iris$Sepal.Length), ]
+head(iris.new)
+iris[order(iris$Species, decreasing = T,
+           iris$Sepal.Length), ] # 정렬기준을 2개설정
+
+
+
+
+# 데이터 분리
+sp <- split(iris, iris$Species)
+sp
+summary(sp)
+summary(sp$setosa)
+sp$setosa
+
+
+
+# 데이터 선택
+subset(iris, Species == 'setosa')
+subset(iris, Sepal.Length > 7.5)
+subset(iris, Sepal.Length > 5.1 & Sepal.Width>3.9)
+subset(iris, Sepal.Length > 7.6,
+       select = c(Petal.Length, Petal.Width))
+
+
+# 데이터 Sampling
+# 숫자를 임의로 추출 (vector)
+# sample(x, size = 10, replace =  FALSE) // 10개씩 추출 replace 는 비복원은 False 뽑은건 안뽑는다.
+x <- 1:100
+y <- sample(x, size = 10, replace =  FALSE)
+y
+
+# 행을 임의로 추출
+idx <- sample( 1:nrow(iris), size = 50, replace = F)
+iris.50 <- iris[idx,]
+dim(iris.50)
+head(iris.50)
+
+
+sample(1:20, size = 5)
+sample(1:20, size = 5)
+sample(1:20, size = 5)
+
+set.seed(16)
+sample(1:20, size = 5)
+
+set.seed(100)
+sample(1:20, size = 5)
+
+set.seed(100)
+sample(1:20, size = 5)
+
+
+
+
+# 데이터조합
+combn(1:5, 3)
+
+x=c("red", "green", "blue", "black","white ")
+com <- combn(x,2)
+com
+
+for (i in i:ncol(com)){
+  cat(com[,i], "\n")
+}
+
+
+
 
 
 
