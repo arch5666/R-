5dff214ac03ef554e60ba7c633aa4b4c4c337615
jinxin4u <jinxin4u@gmail.com>
2019-12-09 13:49:09 +0900

    Update 7.R

--- 7.R
+++ 7.R
@@ -27,9 +27,26 @@ ggplot(df,aes(x=month, y=rain))+        # mapping 생략, mapping 위치 ggplot/
 
 ggplot(df,aes(x=month, y=rain))+
    geom_bar(stat="identity", width=0.7, fill="steelblue")+
- ggtitle("월별 강수량")+
- theme(plot.title=element_text(size=25, face="bold",
+ ggtitle("월별 강수량")+           # title 출력
+ theme(plot.title=element_text(size=25, face="bold",    #
                               colour="steelblue"))+
-labs(x="월", y="강수량")+coord_flip()
+labs(x="월", y="강수량") +coord_flip() #x,y label, 그래프를 가로로(coord_flip)
+
+
+ggplot(iris, aes(x=Petal.Length))+
+  geom_histogram(binwidth = 0.5)       # 연속형 데이터의 빈도 확인(히스토그램), 꽃잎의 길이를  0.5단위로 계산
+      
+                                
+
+# Layer 구조(계층방식, Graphic 처리 ) -> ggplot-> geom_histogram+theme 
+ggplot(iris, aes(x=Sepal.Width, fill=Species,
+                 color=Species))+
+  geom_histogram(binwidth=0.5, position ="dodge")+
+                   theme(legend.position = "top") # Species 범주형, color= 경계선의 색깔 , dodge = 품종별로 각각 그려랴 
+                                                  # 범례 위치 
+
+
+
+
 
 
