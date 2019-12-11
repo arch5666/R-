13bfe491b694d6f7473918fae77c8110182d7223
jinxin4u <jinxin4u@gmail.com>
2019-12-11 11:31:30 +0900

    Update 9.R

---
+++ 9.R
@@ -0,0 +1,70 @@
+# 2019.12.11(수)
+#
+# 12일차
+#
+# 워드클라우드(Word Cloud)
+# 한글 워드클라우드 절차
+#1. Java 실행환경 구축
+#2. 자료 수집(Text자료) 
+#      2.1 text file 형태로 수집  
+#      2.2 web scraping을 이용하여 수집
+# 3. 명사 추출
+Sys.setenv(JAVA_HOME='C:/Program Files/Java/jre1.8.0_231') 
+
+install.packages("wordcloud")  
+library(wordcloud)
+install.packages("wordcloud2")
+library(wordcloud2)
+install.packages("KoNLP")    # 한국어 처리
+library(KoNLP)
+install.packages("RColorBrewer")  #색상처리
+library(RColorBrewer)
+
+library(dplyr)
+library(ggplot2)
+
+setwd("D:/workR")
+text <- readLines("mis_document.txt", encoding='UTF-8') # Text File 마지막에 공백줄!
+text
+
+#'우리 말씀' 한글사전 로딩
+buildDictionary(ext_dic = 'woorimalsam')  
+pal2 <- brewer.pal(8,'Dark2')      # 색상 팔레트 생성
+noun <- sapply(text, extractNoun,USE.NAMES = F) # 명사 추출
+noun
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
